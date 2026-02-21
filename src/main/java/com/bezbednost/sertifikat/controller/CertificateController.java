79b6b325da48011729369ea913ce573b4d265032
HakTak <katek.m536@gmail.com>
2026-02-21 18:26:08 +0100

    one line

--- src/main/java/com/bezbednost/sertifikat/controller/CertificateController.java
+++ src/main/java/com/bezbednost/sertifikat/controller/CertificateController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
